cb547fb52ee7015e035e49b8b0c1787ab47c1a91
kahfei <bak.kahfei@gmail.com>
2015-06-16 13:23:28 +0800

    add controller

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,11 @@
 require 'rulers'
 
 module BestQuotes
-  class Application > Rulers::Application
+  class Application < Rulers::Application
   end
 end
+
+$LOAD_PATH << File.join(File.dirname(__FILE__),
+                        "..", "app",
+                        "controllers")
+require "quotes_controller"
